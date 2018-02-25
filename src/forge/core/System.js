62fce19076fc341a31553d7402b3072e564576e0
Ian Paschal <i.paschal@ultimaker.com>
2018-02-25 17:32:33 +0100

    Added system class

--- src/forge/core/System.js
+++ src/forge/core/System.js
@@ -1,24 +1,53 @@
 // Forge is distributed under the MIT license.
 
+import * as Three from "three";
+import capitalize from "../utils/capitalize";
+
 /**
-	* Class representing an Assembly.
+	* Class representing a System.
 	*/
 class System {
 
 	/**
-		* Create a Component.
+		* Create a System.
+		* @param
 		*/
-	constructor( json ) {
-		console.log( "New system created." );
+	constructor( props, initFn, updateFn ) {
+		this.name = props.name || "unnamed";
+		this.step = props.step || 100;
+		this.fixed = props.fixed || true;
 
-		this._components = json.components;
-		console.log( this );
+		this.inifFn = initFn;
+		this.updateFn = updateFn;
+
+		this._savedTime = 0;
+
+		console.log( "Created a new " + this.name + " system." );
+		return this;
 	}
 
-	getComponents() {
-		return this._components;
+	init( engine ) {
+		if( !engine ) {
+			console.warn( capitalize( this.name ) + ": Attempted to initalize system without an engine!" );
+			return;
+		}
+		console.log( capitalize( this.name ) + ": Linked system to engine." );
+		// Run the actual init behavior:
+		this.initFn();
 	}
 
+	update( delta ) {
+		if ( this.fixed ) {
+			// Add time to the accumulator & simulate if greater than the step size:
+			this._savedTime += delta;
+			if ( this._savedTime >= this.step ) {
+				this.updateFn( this.step );
+				this._savedTime -= this.step;
+			}
+		} else {
+			this.updateFn( delta );
+		}
+	}
 }
 
 export default System;
