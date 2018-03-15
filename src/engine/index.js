397af5482dfd13e4199edfa26f958a9f2060d54f
Ian Paschal <ian.paschal@gmail.com>
2018-03-15 23:04:28 +0100

    Switch to aurora

--- src/engine/index.js
+++ src/engine/index.js
@@ -2,7 +2,7 @@
 
 import Path from "path";
 import { remote } from "electron";
-import Engine from "../../../forge-engine/src/core/Engine";
+import Engine from "../../../aurora/src/core/Engine";
 
 /* Does this file look small? It is. This pattern is copied from Vuex for
 	creating a central store. It's important that we create an instance and then
