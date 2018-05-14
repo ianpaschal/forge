198ab6ae816e882883402bf6e7035cac0b055c2c
Ian Paschal <ian.paschal@gmail.com>
2018-05-14 08:00:58 +0200

    Network Refactor (Part 1)
    - Extending start and end functions for updates to include state management.
    - Added dummy population functsions to play.js.

--- src/play.js
+++ src/play.js
@@ -44,3 +44,50 @@ window.onload = () => {
 		}
 	});
 };
+
+// init( stack, world, onProgress, onFinished ) {
+// 	// Compute total length:
+// 	let length = 0;
+// 	let loaded = 0;
+// 	for ( const section in stack ) {
+// 		length += Object.keys( stack[ section ] ).length;
+// 	}
+// 	if ( world ) {
+// 		length += Object.keys( world.entities ).length;
+// 	}
+// 	function updateProgress( itemName ) {
+// 		loaded++;
+// 		onProgress( ( loaded / length ) * 100, "Loaded " + itemName );
+// 	}
+// 	function onLoadComplete() {
+//
+// 		// Systems must be intialized after loading so they can use assets:
+// 		// this.registerSystem( graphicsSystem );
+// 		this.registerSystem( terrainSystem );
+// 		this.registerSystem( productionSystem );
+// 		this.registerSystem( movementSystem );
+// 		// this.registerSystem( visibilitySystem );
+//
+// 		// this._world = new World();
+// 		// this._world.setTime( 0 );
+//
+// 		// If no source is provided, generate a new world:
+// 		if ( !world ) {
+// 			console.warn( "World is missing, a new one will be generated!" );
+// 			this.testPopulateWorld( 4, updateProgress, onFinished );
+// 		}
+// 		else {
+// 			this.loadWorld( world, updateProgress, onFinished );
+// 		}
+// 	}
+//
+// 	// Load assets. On finished, start the world loading/generation routine:
+// 	// onProgress is called when an asset is loaded and passed the
+// 	if ( stack.length > 0 ) {
+// 		console.log( "Going to load stuff" );
+// 		this.loadAssets( stack, updateProgress, onLoadComplete.bind( this ) );
+// 	}
+// 	else {
+// 		onLoadComplete.bind( this )();
+// 	}
+// }
